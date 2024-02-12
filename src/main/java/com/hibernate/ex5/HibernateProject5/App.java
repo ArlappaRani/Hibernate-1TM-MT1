91cc65391f75f72af3541b354994fc61abafcf44
USER <USER@192.168.100.23>
2024-02-12 10:50:45 +0530

    Hibernate manyToone And One To Many operation

---
+++ src/main/java/com/hibernate/ex5/HibernateProject5/App.java
@@ -0,0 +1,76 @@
+package com.hibernate.ex5.HibernateProject5;
+
+import java.util.*;
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//creating the product objects
+    	Product p1=new Product();
+    	Product p2=new Product();
+    	Product p3=new Product();
+    	Product p4=new Product();
+    	
+    	//creating the list of products
+    	List<Product>list1=new ArrayList<Product>();
+    	List<Product>list2=new ArrayList<Product>();
+    	//adding products in list1 and list2
+    	list1.add(p1);
+    	list1.add(p2);
+    	list2.add(p3);
+    	list2.add(p4);
+    	
+    	//Creating the objects of customer entity class
+    	Customer c1=new Customer(11,"Ajay",23,list1);
+    	Customer c2=new Customer(12,"Amit",24,list2);
+    	
+    	//Setting the values of product to customer
+    	p1.setP_id(1111);
+    	p1.setP_name("Watch");
+    	p1.setP_cost(4500);
+    	p1.setCust(c1);
+    	p2.setP_id(2222);
+    	p2.setP_name("Mobile");
+    	p2.setP_cost(25000);
+    	p2.setCust(c1);
+    	p3.setP_id(3333);
+    	p3.setP_name("Laptop");
+    	p3.setP_cost(85000);
+    	p3.setCust(c2);
+    	p4.setP_id(4444);
+    	p4.setP_name("Bike");
+    	p4.setP_cost(120000);
+    	p4.setCust(c2);
+    	
+       Configuration cfg =new Configuration().configure();
+       SessionFactory factory=cfg.buildSessionFactory();
+       
+       //open session 
+       Session session = factory.openSession();
+       //get transaction
+       Transaction trans = session.beginTransaction();
+       
+       //saving the objects
+       session.save(c1);
+       session.save(c2);
+       session.save(p1);
+       session.save(p2);
+       session.save(p3);
+       session.save(p4);
+       
+       //we are using trans action we have to commit
+       trans.commit();
+       System.out.println("Objects are saved");
+       
+       
+       
+    }    
+}
